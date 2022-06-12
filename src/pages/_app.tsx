2ac7fb59347ea91b2cbca8990c4aca11651b2ab6
douglasmatosdev <douglasmatosdev@gmail.com>
2022-06-12 11:48:15 -0300

    clean project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Head>
+        <title>douglasmatosdev</title>
+        <meta name="description" content="douglasmatosdev website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
