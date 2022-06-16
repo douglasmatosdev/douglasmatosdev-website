d4300080e54f8e165ee81ac1962a3eda19fd18a4
douglasmatosdev <douglasmatosdev@gmail.com>
2022-06-16 17:56:18 -0300

    chore:Footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,19 +7,22 @@ interface FooterProps {
 
 export function Footer({ displayName }: FooterProps) {
     return (
-        <footer className="p-4 fixed bottom-0 left-0 right-0 bg-gray-800 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-            <div className="w-10/12 mx-auto flex justify-between">
+        <footer className="w-full p-4 fixed bottom-0 left-0 right-0 bg-gray-800 flex items-center justify-center md:p-6 dark:bg-gray-800">
+            <div className={`
+            flex w-10/12 mx-auto flex-col jusify-center items-center text-center
+            lg:flex-row md:justify-between
+            `}>
                 <span
-                    className="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+                    className="block text-sm text-gray-500 sm:text-center dark:text-gray-400"
                 >
                     &copy; {new Date().getFullYear()}
                     <Link href={profile.linkedin}>
                         <a className="hover:underline"> {displayName}</a>
                     </Link>
 
-                    . All Rights Reserved.
+               . All Rights Reserved.
                 </span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+                <ul className="flex justify-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
                         <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
                     </li>
